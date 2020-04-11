6716afdf60681f5f4f56377a666083f335890e7e
jacqueskpoty <jacques.kpoty@gmail.com>
2020-04-11 13:04:09 -0400

    desiging the menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomePage.Component';
+import HomePage from './Pages/HomePage/HomePage.Component';
 
 function App() {
   return (
---
+++ src/Pages/HomePage/HomePage.Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../Components/Directory/Directory.Component';
+import './HomePage.Style.scss'
+
+
+const HomePage = () => (
+
+    <div className='homePage'>
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
