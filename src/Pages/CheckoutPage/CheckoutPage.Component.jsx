bf512e84e0abdf4d6500fdac44a4d0aa772b3519
jacqueskpoty <jacques.kpoty@gmail.com>
2020-05-02 03:51:21 -0400

    Stripe integrated

--- src/Pages/CheckoutPage/CheckoutPage.Component.jsx
+++ src/Pages/CheckoutPage/CheckoutPage.Component.jsx
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { cartItemSelector } from '../../Redux/Cart/Cart.Selector';
 import { totalAmountSelector } from '../../Redux/Cart/Cart.Selector';
-import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem.Component'
+import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem.Component';
+import StripeButton from '../../Components/StripeButton/StripeButton.Component';
 
 const Checkout = ({cartItems, totalAmount}) => (
     <div className='checkout-page'>
@@ -32,6 +33,14 @@ const Checkout = ({cartItems, totalAmount}) => (
         <div className='total'>
             <span>Total : ${totalAmount}</span> 
         </div>
+
+        <div className='text-warning'>
+            *Please use the following test credit card for payment*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+        </div>
+
+        <StripeButton price={totalAmount} />
     </div>
 )
 
